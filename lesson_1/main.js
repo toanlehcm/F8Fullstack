b55b8bd4724d7c00fb310269d296f374e586654c
Toan Le <thanhtoanhcm1602@gmail.com>
2024-07-07 05:58:27 +0700

    Modules

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -1,30 +1,7 @@
-var brand = 'F8'
-var course = 'JS Fullstack'
+import logger from './logger.js'
+// import { TYPE_LOG, TYPE_WARN, TYPE_ERROR } from './constants.js'
+import * as constants from './constants.js'
 
-function highlight([first, ...strings], ...values) {
-  var htmlString = values.reduce(function (acc, curr, index) {
-    // console.log(index, acc, curr);
+console.log(constants);
 
-    var eleShift = strings.shift()
-    // console.log(eleShift);
-
-    return acc = `${acc}<span style="color: red">${curr}</span>${eleShift}`
-  }, first)
-
-  // console.log(htmlString)
-
-  var root = document.getElementById('root')
-  root.innerHTML = htmlString
-}
-
-highlight`Hoc lap trinh ${course} tại ${brand} !`
-
-function highlightV2([first, ...strings], ...values) {
-  return values.reduce(
-    (acc, curr) => [...acc, `<span style="color: red">${curr}</span>`, strings.shift()],
-    [first]).join('')
-}
-
-var htmlStringV2 = highlightV2`Hoc lap trinh ${course} tại ${brand} !`
-var rootV2 = document.getElementById('root_v2')
-rootV2.innerHTML = htmlStringV2
+// logger('Hello World', TYPE_LOG)
