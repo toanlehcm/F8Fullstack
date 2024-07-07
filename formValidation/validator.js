e8a4339a1ff2331796db10471ecd12fd14522f64
Toan Le <thanhtoanhcm1602@gmail.com>
2024-07-07 17:08:49 +0700

    - Validate form: password_confirmation. - Custom message. - Validate multiple rules.

--- formValidation/validator.js
+++ formValidation/validator.js
@@ -1,12 +1,22 @@
 /*--------- Constructor function Validator. ------------*/
 function Validator(options) {
 
+  var selectorRules = {}
+
   // Handle validation.
   function validated(inputElement, rule) {
     // Get value in input to check rule.
-    var errorMessage = rule.test(inputElement.value)
+    var errorMessage;
     var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
 
+    // Get all rules of selector.
+    var rules = selectorRules[rule.selector]
+
+    // Loop through all rules and check. If has error, break.
+    for (let i = 0; i < rules.length; i++) {
+      errorMessage = rules[i](inputElement.value)
+      if (errorMessage) break;
+    }
 
     if (errorMessage) {
       // Get parent element of inputElement. Then find span element to show message.
@@ -24,8 +34,28 @@ function Validator(options) {
   var formElement = document.querySelector(options.form);
 
   if (formElement) {
+    //     Handle submit form.
+    formElement.onsubmit = function (e) {
+      e.preventDefault()
+
+      // Loop through each rules and validate.
+      options.rules.forEach(function (rule) {
+        var inputElement = formElement.querySelector(rule.selector);
+        validated(inputElement, rule)
+      });
+    }
+
+
     // Receive rules configed.
+    // Loop through each rules and handle (listen blur event, input,...).
     options.rules.forEach(function (rule) {
+
+      // Save rules for each input.
+      if (Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test)
+      } else {
+        selectorRules[rule.selector] = [rule.test]
+      }
 
       // Ge input from form in options.
       var inputElement = formElement.querySelector(rule.selector);
@@ -34,7 +64,6 @@ function Validator(options) {
         inputElement.onblur = function () {
           // value: get from inputElement.value
           // test: get from rule.test
-          console.log('inputElement', rule);
           // Handle for case blur outside input element.
           validated(inputElement, rule)
 
@@ -54,33 +83,42 @@ function Validator(options) {
 // Principle of rules:
 // 1. when have error : return message error.
 // 2. when validated : return undefined.
-Validator.isRequired = function (selector) {
+Validator.isRequired = function (selector, message) {
   return {
     selector: selector,
     test: function (value) { // Receive value from input.
 
-      return value?.trim() ? undefined : 'Vui lòng nhập trường này.'
+      return value?.trim() ? undefined : message || 'Vui lòng nhập trường này.'
     }
   }
 }
 
-Validator.isEmail = function (selector) {
+Validator.isEmail = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
       var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-      return regex.test(value) ? undefined : 'Vui lòng nhập email.'
+      return regex.test(value) ? undefined : message || 'Vui lòng nhập email.'
     }
   }
 }
 
-Validator.minLength = function (selector, min) {
+Validator.minLength = function (selector, min, message) {
   return {
     selector: selector,
     test: function (value) {
-      console.log('value', value);
-      return value.length >= min ? undefined : `Vui lòng nhập nhiều hơn ${min} kí tự.`
+      return value.length >= min ? undefined : message || `Vui lòng nhập nhiều hơn ${min} kí tự.`
+    }
+  }
+}
+
+// getConfirmValue is a callback.
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value === getConfirmValue() ? undefined : message || 'Giá trị nhập lại không đúng.'
     }
   }
 }
